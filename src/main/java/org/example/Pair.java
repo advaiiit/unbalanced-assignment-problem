685b3b1345062ea2108cbaf0b4175b10d82def5c
Advait Kamathe <advaitkamathe@gmail.com>
2022-12-12 21:56:34 +0530

    Creating Class Pair

---
+++ src/main/java/org/example/Pair.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Pair {
+
+    private String key;
+    private Integer value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
